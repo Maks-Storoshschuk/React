dbc4caa491bc7ca41dafa35e2f1eb5a29ffc089a
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-08-11 18:13:27 +0300

    3/3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Users from "./components/Users/Users";
 import Posts from "./components/posts/Posts";
-import Coment from "./components/coment/Coment";
 import Coments from "./components/coments/Coments";
 
 export default function App(){
