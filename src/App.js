946671d643851bbacacebf5343451d8a72e27a2d
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-08-19 14:45:03 +0300

    всі авто

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+    BrowserRouter as Router,
+    Link,
+    Route
+} from "react-router-dom";
 import './App.css'
-import Auto from "./auto/auto";
+import Autos from "./auto/auto";
 export default function App(){
     return(
-        <div>
-            <Auto/>
-        </div>
+        <Router>
+            <div>
+                <Link to={'/'}>Всі авто</Link>
+                <Link to={'/even'}>Авто з парною кількістю літер</Link>
+                <Link to={'/odd'}>Авто з непарною кількістю літер</Link>
+            </div>
+            <Route path={'/'}>
+                <Autos/>
+            </Route>
+        </Router>
+
     )
 }
+App()
--- src/auto/auto.js
+++ src/auto/auto.js
@@ -1,12 +1,13 @@
 import {useEffect, useState} from "react";
 import {getAuto} from "../auto.services/auto.services";
 import Car from "./car";
-export default function Auto(){
+export default function Autos(){
     let [cars,setCars] = useState([])
     useEffect(()=>{
         getAuto().then(value=>{
             setCars([...value])
         })
+        cars.filter()
     },[]);
     return(
         <div className={'judas'}>
