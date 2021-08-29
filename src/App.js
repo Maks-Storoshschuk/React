85c34d750620e14a7de612974913549cad8b9dec
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-08-29 18:33:07 +0300

    кнопки+форма+lear store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {fetchUsers} from "./services/users.api";
-import {FETCH_USERS} from "./redux/actions";
+import {addUser, fetchUsers} from "./services/users.api";
+import {CLEAR_STORE, fetch_Users, pushUser} from "./redux/actions";
 
 export default function App(){
     let state = useSelector(state =>state);
     let dispatch = useDispatch();
     let {users}=state;
 useEffect(()=>{
-    fetchUsers().then(value => {console.log(value);
-    dispatch({type: FETCH_USERS,payload:value})
+    fetchUsers().then(value => {
+        dispatch(fetch_Users(value))
     });
 },[])
+    let onSubmit = (e)=>{
+    e.preventDefault();
+        let name = e.target.name.value;
+        let user = {name};
+        addUser(user).then(value => {
+            console.log('saved user',value);
+            dispatch(pushUser(value))
+        })
+    };
+    let onClickC = ()=>{
+        dispatch({type: CLEAR_STORE});
+    };
+    let onClickS=()=>{
+        fetchUsers().then(value => {console.log(value);
+            dispatch(fetch_Users(value))
+        });
+    };
     return(
         <div>
+            <form onSubmit={onSubmit}>
+                <input type='text' name={'name'}/>
+                <button>add user</button>
+            </form>
+            <button onClick={onClickC}>
+                clear users
+            </button>
+            <button onClick={onClickS}>
+                set users to store
+            </button>
             {
-                users.map((value)=><div>{value.name}</div>)
+                users.map((value)=><div key={value.id}>{value.id}. {value.name}</div>)
             }
         </div>
     )
--- src/services/users.api.js
+++ src/services/users.api.js
@@ -2,4 +2,15 @@ const fetchUsers = ()=>{
     return fetch('https://jsonplaceholder.typicode.com/users')
         .then(value => value.json())
 }
-export {fetchUsers}
+
+const addUser = (user)=>{
+    return fetch('https://jsonplaceholder.typicode.com/users',{
+        method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(value => value.json())
+}
+export {fetchUsers,addUser};
