fdc93b804f4f2f32058a4f4ca723fe9c992ba36d
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-09-09 11:49:31 +0300

    змінив стилізацію мобільної версії

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,11 @@
 import Movie from "../MoviesList/movies";
 import {Switch, Route, Link} from "react-router-dom";
 import {MovieInfo} from "../MovieInfo/movieInfo";
-import GenresMovie from "../GenreBadge/genreMovie";
+import GenresMovie from "../GenreStaff/genreMovie";
 import logo from  './Pinball.PNG'
 import user from '../UserInfo/Borat.jpg'
 import './header.css'
-import Genres from "../GenreBadge/genres";
+import Genres from "../GenreStaff/genres";
 import {useState} from "react";
 console.log(logo)
 export default function Header(){
--- src/Components/MoviesListCard/moviescard.js
+++ src/Components/MoviesListCard/moviescard.js
@@ -1,6 +1,6 @@
 import {Link,} from "react-router-dom";
 import Stars from "../StarsRating/star-rating";
-import {GenreName} from "../GenreBadge/nameSelectMovieGenre";
+import {GenreName} from "../GenreStaff/nameSelectMovieGenre";
 import './moviescard.css'
 
 export default function MoviesCard({item}){
@@ -8,8 +8,11 @@ export default function MoviesCard({item}){
         <div className={"MOCA"}>
             <Link  to={{pathname: `/id:${item.id}`, state: item}}><img
                 src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="eror"/>
-                <h2 >{item.title}</h2>
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`} alt="eror"/>
             </Link>
+            <h2 >{item.title}</h2>
+            <i>{item.overview}</i>
             <Stars item={item.vote_average} />
             <div className={'genre_under_movies'}>
                 <GenreName genre_ids={item.genre_ids}/>
